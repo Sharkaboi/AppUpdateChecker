1a9155f57b0a7539a93177af643fd57683c8998d
Sharkaboi <cybersharkbusiness@gmail.com>
2024-03-24 11:53:39 +0530

    Add better exceptions sample

--- sample/src/main/java/com/github/sharkaboi/appupdatechecker/sample/MainActivity.kt
+++ sample/src/main/java/com/github/sharkaboi/appupdatechecker/sample/MainActivity.kt
@@ -5,6 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.github.sharkaboi.appupdatechecker.sample.databinding.ActivityMainBinding
 import com.sharkaboi.appupdatechecker.AppUpdateChecker
+import com.sharkaboi.appupdatechecker.models.AppUpdateCheckerException
+import com.sharkaboi.appupdatechecker.models.GenericError
+import com.sharkaboi.appupdatechecker.models.InvalidEndPointException
+import com.sharkaboi.appupdatechecker.models.InvalidPackageNameException
+import com.sharkaboi.appupdatechecker.models.InvalidRepositoryNameException
+import com.sharkaboi.appupdatechecker.models.InvalidUserNameException
+import com.sharkaboi.appupdatechecker.models.InvalidVersionException
+import com.sharkaboi.appupdatechecker.models.PackageNotFoundException
+import com.sharkaboi.appupdatechecker.models.RemoteError
 import com.sharkaboi.appupdatechecker.models.UpdateResult
 import com.sharkaboi.appupdatechecker.sources.github.GithubTagSource
 import kotlinx.coroutines.CoroutineExceptionHandler
@@ -13,8 +22,44 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val errorHandler = CoroutineExceptionHandler { _, throwable ->
+        if (throwable is AppUpdateCheckerException) {
+            when (throwable) {
+                is GenericError -> {
+                    // Handle explicitly if needed
+                }
+
+                is InvalidEndPointException -> {
+                    // Handle explicitly if needed
+                }
+
+                is InvalidPackageNameException -> {
+                    // Handle explicitly if needed
+                }
+
+                is InvalidRepositoryNameException -> {
+                    // Handle explicitly if needed
+                }
+
+                is InvalidUserNameException -> {
+                    // Handle explicitly if needed
+                }
+
+                is InvalidVersionException -> {
+                    // Handle explicitly if needed
+                }
+
+                is PackageNotFoundException -> {
+                    // Handle explicitly if needed
+                }
+
+                is RemoteError -> {
+                    // Handle explicitly if needed
+                }
+            }
+        }
+
         binding.tvUpdateDetails.text =
-            "Error occurred when checking for updates:\n${throwable.message}"
+            "Error occurred when checking for updates:\n\n$throwable"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
